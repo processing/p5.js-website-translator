63ee5dfbf9dd87921fd1b898530aabbb87d375d5
yukienomiya <nomiyayukie@gmail.com>
2020-07-24 19:02:25 +0200

    Add textCard components to Reference page

--- pages/reference.vue
+++ pages/reference.vue
@@ -1,11 +1,35 @@
 <template>
   <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
-      <v-card>
-        <v-card-title class="headline">
-          Reference
-        </v-card-title>
-      </v-card>
+    <v-flex>
+      <textCard
+        v-for="(item, key) in data"
+        :key="key"
+        :str-key="key"
+        :original-str="item"
+      />
     </v-flex>
   </v-layout>
 </template>
+
+<script>
+import textCard from '../components/textCard.vue'
+export default {
+  components: {
+    textCard,
+  },
+  async fetch() {
+    const flatten = require('flat')
+    const enJSON = await this.$http.$get(
+      `https://raw.githubusercontent.com/processing/p5.js-website/d27452f34f5ed493b27c1b5fc37a65a655ae699d/src/data/reference/en.json`
+    )
+    const flattenedEnJSON = flatten(enJSON)
+    this.data = flattenedEnJSON
+  },
+  data() {
+    return {
+      data: {},
+    }
+  },
+  fetchOnServer: false,
+}
+</script>
