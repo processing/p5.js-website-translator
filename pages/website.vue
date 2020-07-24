bd6b1ce745bdc040fbd0c97554ae0a7dcdbf58e4
yukienomiya <nomiyayukie@gmail.com>
2020-07-24 19:02:52 +0200

    Update website.vue

--- pages/website.vue
+++ pages/website.vue
@@ -1,11 +1,27 @@
 <template>
   <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
-      <v-card>
-        <v-card-title class="headline">
-          Website
-        </v-card-title>
-      </v-card>
-    </v-flex>
+    <h1>{{ dataJSON }}</h1>
   </v-layout>
 </template>
+
+<script>
+/* import textCard from '../components/textCard.vue' */
+import YAML from 'yaml'
+export default {
+  async fetch() {
+    const dataYAML = await this.$http.$get(
+      `https://raw.githubusercontent.com/yukienomiya/p5.js-website/italian/src/data/it.yml`
+    )
+    this.dataJSON = YAML.parseDocument(dataYAML)
+  },
+  /* components: {
+    textCard,
+  }, */
+  data() {
+    return {
+      dataJSON: {},
+    }
+  },
+  fetchOnServer: false,
+}
+</script>
