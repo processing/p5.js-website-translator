ffc76942184a630ac4f5165b5416058a1e306bdb
yukienomiya <nomiyayukie@gmail.com>
2020-07-30 11:50:31 +0200

    Update reference.vue

--- pages/reference.vue
+++ pages/reference.vue
@@ -12,16 +12,16 @@
 </template>
 
 <script>
+import flatten from 'flat'
 import textCard from '../components/textCard.vue'
 export default {
   components: {
     textCard,
   },
   async fetch() {
-    const flatten = require('flat')
-    const enJSON = await this.$http.$get(
-      `https://raw.githubusercontent.com/processing/p5.js-website/d27452f34f5ed493b27c1b5fc37a65a655ae699d/src/data/reference/en.json`
-    )
+    const jsonURL =
+      'https://raw.githubusercontent.com/processing/p5.js-website/main/src/data/en.yml'
+    const enJSON = await this.$http.$get(jsonURL)
     const flattenedEnJSON = flatten(enJSON)
     this.data = flattenedEnJSON
   },
