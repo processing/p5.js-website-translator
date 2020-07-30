25b4809db39d45ee0825457159a6b92274c1432e
yukienomiya <nomiyayukie@gmail.com>
2020-07-30 11:57:04 +0200

    Add textCard components to Website page

--- pages/website.vue
+++ pages/website.vue
@@ -1,25 +1,47 @@
 <template>
   <v-layout column justify-center align-center>
-    <h1>{{ dataJSON }}</h1>
+    <v-flex>
+      <textCard
+        v-for="(item, key) in data"
+        :key="key"
+        :str-key="key"
+        :original-str="item"
+      />
+    </v-flex>
   </v-layout>
 </template>
 
 <script>
-/* import textCard from '../components/textCard.vue' */
-import YAML from 'yaml'
+import yaml from 'js-yaml'
+import flatten from 'flat'
+import textCard from '../components/textCard.vue'
 export default {
-  async fetch() {
-    const dataYAML = await this.$http.$get(
-      `https://raw.githubusercontent.com/yukienomiya/p5.js-website/italian/src/data/it.yml`
-    )
-    this.dataJSON = YAML.parseDocument(dataYAML)
-  },
-  /* components: {
+  components: {
     textCard,
-  }, */
+  },
+  async fetch() {
+    const yamlURL =
+      'https://raw.githubusercontent.com/processing/p5.js-website/main/src/data/en.yml'
+    let dataYAML
+    // eslint-disable-next-line no-useless-catch
+    try {
+      dataYAML = await this.$http.$get(yamlURL)
+    } catch (error) {
+      throw error
+      // TODO add dialog with error msg and reload button
+    }
+    let data
+    // eslint-disable-next-line no-useless-catch
+    try {
+      data = yaml.safeLoad(dataYAML)
+    } catch (error) {
+      throw error
+    }
+    this.data = flatten(data)
+  },
   data() {
     return {
-      dataJSON: {},
+      data: {},
     }
   },
   fetchOnServer: false,
