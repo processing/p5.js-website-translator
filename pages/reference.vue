bb0e653b6424d248f4d07975322c63ecb858712d
yukienomiya <nomiyayukie@gmail.com>
2020-08-31 11:14:18 +0200

    Update reference.vue
    Add toolBar component
    Use local storage for storing files and changes

--- pages/reference.vue
+++ pages/reference.vue
@@ -1,5 +1,6 @@
 <template>
   <v-layout column justify-center align-center>
+    <tool-bar page="reference" @setLang="setLanguage($event)"></tool-bar>
     <virtual-list
       class="list"
       style="height: 100vh; overflow-y: auto; width: 100%;"
@@ -13,29 +14,121 @@
 </template>
 
 <script>
-import flatten from 'flat'
+import flat from 'flat'
 import VirtualList from 'vue-virtual-scroll-list'
 import textCard from '../components/textCard.vue'
+import toolBar from '../components/toolBar.vue'
+import actions from '../plugins/actions.js'
 export default {
-  components: { 'virtual-list': VirtualList },
+  components: { 'virtual-list': VirtualList, 'tool-bar': toolBar },
   async fetch() {
-    const jsonURL =
-      'https://raw.githubusercontent.com/processing/p5.js-website/82b966d964d34db9d7a0d2bf436ff9a3754e08ca/src/data/reference/en.json'
-    const enJSON = await this.$http.$get(jsonURL)
-    const flattenedEnJSON = flatten(enJSON)
-    const data = []
-    for (const key in flattenedEnJSON) {
-      const obj = { strKey: key, originalStr: flattenedEnJSON[key] }
-      data.push(obj)
+    let flattenedOriginalFile = {}
+    const originalFileLocalStorageKey =
+      'p5js-website-translator.files.reference.en'
+    const originalFileLocalStorage = await this.$localForage.getItem(
+      originalFileLocalStorageKey
+    )
+    // if the English file is already saved in the local storage
+    if (originalFileLocalStorage) {
+      flattenedOriginalFile = originalFileLocalStorage.file
+    } else {
+      const originalFileURL =
+        'https://raw.githubusercontent.com/processing/p5.js-website/main/src/data/reference/en.json'
+      const originalFile = await this.$http.$get(originalFileURL)
+
+      const originalFileCommitURL =
+        'https://api.github.com/repos/processing/p5.js-website/commits?path=src/data/reference/en.json'
+      const originalFileCommit = await this.$http.$get(originalFileCommitURL)
+      const originalFileCommitSHA = originalFileCommit[0].sha
+      flattenedOriginalFile = flat.flatten(originalFile, { delimiter: '/' })
+
+      const originalFileValue = {
+        commitSHA: originalFileCommitSHA,
+        file: flattenedOriginalFile,
+      }
+      // save the English file in the local storage
+      await this.$localForage.setItem(
+        originalFileLocalStorageKey,
+        originalFileValue
+      )
     }
-    this.data = data
+
+    let flattenedTranslatedFile = {}
+    // if a language is selected
+    if (this.language) {
+      const translatedFileLocalStorageKey =
+        'p5js-website-translator.files.reference.' + this.language
+      const translatedFileLocalStorage = await this.$localForage.getItem(
+        translatedFileLocalStorageKey
+      )
+      // if the translated file is already saved in the local storage
+      if (translatedFileLocalStorage) {
+        const changesLocalStorageKey =
+          'p5js-website-translator.changes.reference.' + this.language
+        const changesLocalStorage = await this.$localForage.getItem(
+          changesLocalStorageKey
+        )
+
+        flattenedTranslatedFile = actions.buildEditedTranslatedFile(
+          translatedFileLocalStorage.file,
+          changesLocalStorage
+        )
+      } else {
+        const translatedFileURL =
+          'https://raw.githubusercontent.com/processing/p5.js-website/main/src/data/reference/' +
+          this.language +
+          '.json'
+        const translatedFile = await this.$http.$get(translatedFileURL)
+
+        const translatedFileCommitURL =
+          'https://api.github.com/repos/processing/p5.js-website/commits?path=src/data/reference/' +
+          this.language +
+          '.json'
+        const translatedFileCommit = await this.$http.$get(
+          translatedFileCommitURL
+        )
+        const translatedFileCommitSHA = translatedFileCommit[0].sha
+        flattenedTranslatedFile = flat.flatten(translatedFile, {
+          delimiter: '/',
+        })
+        const translatedFileValue = {
+          commitSHA: translatedFileCommitSHA,
+          file: flattenedTranslatedFile,
+        }
+        // save the English file in the local storage
+        await this.$localForage.setItem(
+          translatedFileLocalStorageKey,
+          translatedFileValue
+        )
+        // save the changes object in the local storage
+        const translatedFileChangesLocalStorageKey =
+          'p5js-website-translator.changes.reference.' + this.language
+        await this.$localForage.setItem(
+          translatedFileChangesLocalStorageKey,
+          {}
+        )
+      }
+    }
+    this.data = actions.buildCardsData(
+      this.language,
+      'reference',
+      flattenedOriginalFile,
+      flattenedTranslatedFile
+    )
   },
   data() {
     return {
       item: textCard,
       data: [],
       strKey: '',
+      language: '',
     }
+  },
+  methods: {
+    setLanguage(l) {
+      this.language = l
+      this.$nuxt.refresh()
+    },
   },
   fetchOnServer: false,
 }
